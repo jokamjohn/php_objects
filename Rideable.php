95d8c4a1a11910f6852d3b2feb4b4312190b6fd3
jokamjohn <johnkagga@gmail.com>
2015-08-01 11:21:39 +0300

    interfaces

---
+++ Rideable.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JohnKagga
+ * Date: 8/1/2015
+ * Time: 11:09 AM
+ */
+
+interface Rideable
+{
+    //create an empty function/method
+    public function howToRide();
+}
+
---
+++ Skateboard.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DELL
+ * Date: 8/1/2015
+ * Time: 11:20 AM
+ */
+
+class Skateboard implements Rideable {
+    public function howToRide (){
+        $steps = array();
+        $steps[] = "Balance your foot foot on the board";
+        $steps[] = "Kick,then push with the other foot";
+        $steps[] = "Don`t fall down";
+        return $steps;
+    }
+
+}
